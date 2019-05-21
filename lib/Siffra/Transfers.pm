bfe12c82baa8ba55b8084bea5d9429232b53df36
Luiz Benevenuto <luiz@siffra.com.br>
2019-05-21 10:17:41 -0300

    gp

--- lib/Siffra/Transfers.pm
+++ lib/Siffra/Transfers.pm
@@ -31,7 +31,7 @@ BEGIN
     require Siffra::Tools;
     use Exporter ();
     use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
-    $VERSION = '0.03';
+    $VERSION = '0.04';
     @ISA     = qw(Siffra::Tools Exporter);
 
     #Give a hoot don't pollute, do not export more than needed by default
